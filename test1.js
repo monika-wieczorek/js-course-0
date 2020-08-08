f2933e4ac9b6ae11cf2d7e988fb20b9c4d1b3e6f
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-08 13:17:48 +0200

    testing if, else if statements in JavaScript

---
+++ test1.js
@@ -0,0 +1,27 @@
+var youLikeMeat = false;
+var myNum = 10;
+
+if (youLikeMeat){
+
+    document.write("Here is the meaty menu...");
+
+} else{
+
+    document.write("Here is the vege menu....");
+}
+
+var myAge = 49;
+
+if (myAge > 30){
+
+    document.write("You are over 30");
+} else if (myAge > 20){
+
+    document.write("You are over 20");
+} else if (myAge > 10) {
+
+    document.write("You are over 10");
+} else{
+
+    document.write("You are not over 10");
+}
