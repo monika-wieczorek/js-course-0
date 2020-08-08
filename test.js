e88830a5c2d0d158ece36687d60b11e97e4a2fc3
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-08 11:31:46 +0200

    basic rules of js added to test.js in comments

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 alert("Hello, World!");
+
+/*this is two line
+comment in js*/
+
+//this is a single line comment
+
+//Basic rules of JavaScript
+/*
+*JavaScript is case sensitive
+*Contains statements ending with ;
+*is not sensitive to whitespaces and line breaks
+*oneline comments //
+*multiple line comments 
+*JavaScript runs from top to bottom
+*/
