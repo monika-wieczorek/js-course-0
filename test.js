2413ac7f36af97ba31a3dfc6dd593f0a91145db6
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-08 12:04:25 +0200

    short-hand math operators added to test.js

--- test.js
+++ test.js
@@ -13,4 +13,37 @@ comment in js*/
 *oneline comments //
 *multiple line comments 
 *JavaScript runs from top to bottom
+*JS has dinamic types as they can change in the single variable - although it might be confusing
+*we do not need to specify type of var when we initialize it
 */
+
+//Variables in JS
+
+var myVariable1;
+myVariable1 = 10;
+
+var myVariable2 = "Hello";
+
+//Basic Math Operators
+var myVar = 5; //assignment operator
+5 + 5 //addition operator, may work also as concatenation
+10 - 5 // substruction operator
+10*5 //multiplication operator
+20/10 //division operator
+"Hello" + " " + "David"
+
+//short-hand Math Methods
+myVar += 5 //take myVar, add 5 to it and assing new number to myVar (myVar += 5) = (myVar = myVar+5)
+myVar *= 5 // the same as myVar=myVar*5
+myVar /= 5 // the same as myVar=myVar/5
+myVar -= 5 // the same as myVar=myVar-5
+
+myVar++ // displays myVar and then add 1 to it
+++myVar // add 1 to myVar and displays the sum
+
+--myVar // substract 1 from myVar
+
+
+
+
+
