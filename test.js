8ffd1b3b54fee5464e8f3198df588be851137a39
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-08 13:00:00 +0200

    booleans explanation added to test.js

--- test.js
+++ test.js
@@ -26,22 +26,46 @@ var myVariable2 = "Hello";
 
 //Basic Math Operators
 var myVar = 5; //assignment operator
-5 + 5 //addition operator, may work also as concatenation
-10 - 5 // substruction operator
-10*5 //multiplication operator
-20/10 //division operator
-"Hello" + " " + "David"
+5 + 5; //addition operator, may work also as concatenation
+10 - 5; // substruction operator
+10*5; //multiplication operator
+20/10; //division operator
+"Hello" + " " + "David";
 
 //short-hand Math Methods
-myVar += 5 //take myVar, add 5 to it and assing new number to myVar (myVar += 5) = (myVar = myVar+5)
-myVar *= 5 // the same as myVar=myVar*5
-myVar /= 5 // the same as myVar=myVar/5
-myVar -= 5 // the same as myVar=myVar-5
+myVar += 5; //take myVar, add 5 to it and assing new number to myVar (myVar += 5) = (myVar = myVar+5)
+myVar *= 5; // the same as myVar=myVar*5
+myVar /= 5; // the same as myVar=myVar/5
+myVar -= 5; // the same as myVar=myVar-5
 
-myVar++ // displays myVar and then add 1 to it
-++myVar // add 1 to myVar and displays the sum
+myVar++; // displays myVar and then add 1 to it
+++myVar; // add 1 to myVar and displays the sum
 
---myVar // substract 1 from myVar
+--myVar; // substract 1 from myVar
+
+myVar = "Monika";
+
+/*"document" = object and "write" = method*/ 
+document.write(myVar); // prints out the message on a webpage
+console.log(myVar); // prints out the message to the console
+
+document.write(100/5);
+
+
+//booleans in JavaScript
+/*are used to check conditions of certains scenarios*/
+// single "=" assigns a value to the variable
+// double "==" compares two values, acts as equal sign
+
+Boolean(7 > 5); // checks truesy/falsy of certain elements/values
+Boolean(0); // is the only number in JS that is false
+
+/* Boolean("hello") = true, but Boolean("") = false*/
+
+
+
+
+
 
 
 
