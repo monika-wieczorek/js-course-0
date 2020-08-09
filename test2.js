b45234e43cbafc79aa5857cf9e9f6b51b8c5be5a
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-09 11:23:56 +0200

    THIS keyword and constructor function in test2.js

--- test2.js
+++ test2.js
@@ -22,6 +22,10 @@ var myCar2 = {
     driver: "Piotr",
     drive: function(){
         console.log("now you are driving again");
+    },
+
+    test2: function(){
+        console.log(this);
     }
 };
 
@@ -30,10 +34,46 @@ console.log(myCar2.driver);
 myCar2.drive();
 
 var myCar3 = {
+    driver: "Krzysztof",
     drive: function(speed, time){
         console.log("you have traveled the distance of: ");
         console.log(speed * time);
+    },
+    logDriver: function() {
+        console.log("Driver name is " + this.driver);        
     }
-}
+};
 
 myCar3.drive(50, 3);
+myCar2.test2();
+myCar3.logDriver();
+
+var myArray = new Array(); //constructor function that creats new array
+
+var Bike = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
+        console.log("you have traveled the distance of: ");
+        console.log(speed * time);
+    };
+    this.logDriver = function() {
+        console.log("Driver name is " + this.driver);        
+    };
+    
+}
+
+var myBike = new Bike(20, "Monika");
+var myBike1 = new Bike(10, "Piotr");
+var myBike2 = new Bike(12, "Kasia");
+var myBike3 = new Bike(15, "Paweł");
+var myBike4 = new Bike(17, "Ania");
+
+myBike3.logDriver();
+
+
+
+
+
+
