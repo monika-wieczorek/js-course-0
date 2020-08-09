d2850ca167ca8ec24b88ceac2f3658fd01e4164a
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-09 20:38:03 +0200

    alternative order of script in index1.html

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function setUpEvents(){
 
-button.onclick = function(){
-    if (content.className == "open"){
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else{
-        content.className = "open";
-        button.innerHTML = "Show Less";
-    } 
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    
+    button.onclick = function(){
+        if (content.className == "open"){
+            content.className = "";
+            button.innerHTML = "Show More";
+        } else{
+            content.className = "open";
+            button.innerHTML = "Show Less";
+        } 
+    };
 }
+
+window.onload = function(){
+
+    setUpEvents();
+
+};
+
