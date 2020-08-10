ea5f1c9d49eb446a08adb0ce1509b75ec9b9a81b
Monika Wieczorek <cendrowska.monika@gmail.com>
2020-08-10 09:32:33 +0200

    simple form with js events added

---
+++ script3.js
@@ -0,0 +1,26 @@
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
+
+myForm.name.onfocus = function(){
+    myForm.name.style.border = "4px solid pink";
+};
+
+myForm.name.onblur = function(){
+    myForm.name.style.border = "none";
+};
+
+myForm.onsubmit = function(){
+    
+    if (myForm.name.value == ""){
+
+        message.innerHTML = "please enter a name";
+        return false; // form is stopped from being submitted
+
+    } else{
+
+        message.innerHTML = "";
+        return true;
+
+    }
+
+};
